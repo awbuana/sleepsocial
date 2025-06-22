d7db9bacfc16348ffea865d30cd57944191d3f14
Agni Wira Buana <agni.wira@bukalapak.com>
2025-06-23 00:07:13 +0700

    feat: standardize follows controllers

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,19 @@
 class FollowsController < ApplicationController
   before_action :set_follow, only: %i[ show ]
+  before_action :authenticate!, only: %i[ create destroy ]
 
   # GET /follows
   def index
     permitted = params.permit(:user_id, :limit, :after, :before).to_h.symbolize_keys
     permitted[:limit] = permitted[:limit].to_i if permitted[:limit]
-    raise Sleepsocial::Error.("Parameter user_id is missing") unless permitted[:user_id]
-
     pagination_params = permitted.slice(:limit, :after, :before)
-    paginator = Follow.where(user_id: permitted[:user_id]).cursor_paginate(**pagination_params)
+
+    paginator = if current_user
+      Follow.where(user_id: current_user.id).cursor_paginate(**pagination_params)
+    else
+      Follow.all.cursor_paginate(**pagination_params)
+    end
+
     page = paginator.fetch
 
     render_serializer page.records, FollowSerializer, meta: {
@@ -24,16 +29,14 @@ class FollowsController < ApplicationController
 
   # POST /follows
   def create
-    @user = User.find(follow_params[:user_id])
-    @follow = FollowService.follow(@user, follow_params[:target_user_id])
+    @follow = FollowService.follow(current_user, follow_params[:target_user_id])
 
     render_serializer @follow, FollowSerializer, status: :created
   end
 
   # DELETE /follows
   def destroy
-    @user = User.find(follow_params[:user_id])
-    FollowService.unfollow(@user, follow_params[:target_user_id])
+    FollowService.unfollow(current_user, follow_params[:target_user_id])
 
     render_message "Unfollow successfully"
   end
@@ -47,6 +50,6 @@ class FollowsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def follow_params
-    @follow_params ||= params.expect(follow: [ :user_id, :target_user_id ])
+    @follow_params ||= params.expect(follow: [ :target_user_id ])
   end
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,4 @@
 class Follow < ApplicationRecord
-  include IdentityCache
-
   belongs_to :user
   belongs_to :target_user, class_name: "User"
 end
