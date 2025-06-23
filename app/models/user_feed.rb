fbf746cd33516acb386409c1ffecd0f2c9099a7f
Agni Wira Buana <agni.wira@bukalapak.com>
2025-06-23 19:06:59 +0700

    feat: refactor redis sorted members

--- app/models/user_feed.rb
+++ app/models/user_feed.rb
@@ -1,25 +1,33 @@
 class UserFeed
 
+  class Member
+    attr_reader :id, :user_id, :created_at
+
+    def initialize(id, user_id, created_at)
+      @id = id
+      @user_id = user_id
+      @created_at = created_at
+    end
+  end
+
   def initialize(user)
     @user = user
   end
 
   def feed
-    @feed ||= REDIS.call("ZREVRANGE", feed_key, 0, -1)
+    @feed ||= begin
+      members = REDIS.call("ZREVRANGE", feed_key, 0, -1)
+      members.map{ |m| parse_member(m) }
+    end
   end
 
   def add_to_feed(sleep_log)
     REDIS.call("ZADD", feed_key, score(sleep_log), member_key(sleep_log))
   end
 
-  def remove_from_feed(member_keys)
+  def remove_keys_from_feed(members)
+    member_keys = members.map{ |m| member_key(m) }
     REDIS.call("ZREM", feed_key, member_keys)
-  end
-
-  def self.parse_member(member_key)
-    k, ts = member_key.split("#")
-
-    [k, Time.parse(ts)]
   end
 
   private
@@ -28,11 +36,18 @@ class UserFeed
     "feed:#{@user.id}"
   end
 
+  def parse_member(member_key)
+    res = JSON.parse(member_key)
+    res["ts"] = Time.parse(res["ts"])
+    
+    Member.new(res['id'], res['uid'], res['ts'])
+  end
+
   def score(sleep_log)
     sleep_log.sleep_duration
   end
 
   def member_key(sleep_log)
-    "#{sleep_log.id}##{sleep_log.created_at}"
+    {uid: sleep_log.user_id, id: sleep_log.id, ts: sleep_log.created_at.strftime("%Y%m%dT%H%M%S%z")}.to_json
   end
 end
--- app/services/timeline_service/precomputed_feed.rb
+++ app/services/timeline_service/precomputed_feed.rb
@@ -9,28 +9,29 @@ module TimelineService
 
       logs = user_feed.feed
       deprecated_logs = remove_deprecated_logs(user_feed, logs)
-      filtered_logs = filter_logs(logs, deprecated_logs)
+      filter_log_ids = filter_log_ids(logs, deprecated_logs)
 
-      logs = SleepLog.fetch_multi(filtered_logs, includes: :user)
-      logs.sort_by { |log| [log.sleep_duration, log.id] }.reverse
+      sleep_logs = SleepLog.fetch_multi(filter_log_ids, includes: :user)
+      sleep_logs.sort_by { |log| [log.sleep_duration, log.id] }.reverse
     end
 
     private
 
     def remove_deprecated_logs(user_feed, logs)
       deprecated_logs = logs.select do |log|
-        _, ts = UserFeed.parse_member(log)
-        ts < timeline_threshold
+        log.created_at < timeline_threshold
       end
 
-      user_feed.remove_from_feed(deprecated_logs) if deprecated_logs.present?
+      user_feed.remove_keys_from_feed(deprecated_logs) if deprecated_logs.present?
 
       deprecated_logs
     end
 
-    def filter_logs(logs, deprecated_logs)
-      set_logs = deprecated_logs.to_set
-      logs.reject { |log| set_logs.include?(log) }
+    def filter_log_ids(logs, deprecated_logs)
+      deprecated_keys = deprecated_logs.map{|log| log.id }.to_set
+      logs.reject! { |log| deprecated_keys.include?(log.id) }
+
+      logs.map{ |log| log.id }
     end
 
     def timeline_threshold
