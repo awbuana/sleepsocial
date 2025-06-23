7d8fd3961dd30abf6c9f844f2f566355892c32fc
Agni Wira Buana <agni.wira@bukalapak.com>
2025-06-23 18:20:02 +0700

    feat: added remove old post functionality

--- app/models/user_feed.rb
+++ app/models/user_feed.rb
@@ -9,12 +9,30 @@ class UserFeed
   end
 
   def add_to_feed(sleep_log)
-    REDIS.call("ZADD", feed_key, sleep_log.sleep_duration, sleep_log.id)
+    REDIS.call("ZADD", feed_key, score(sleep_log), member_key(sleep_log))
+  end
+
+  def remove_from_feed(member_keys)
+    REDIS.call("ZREM", feed_key, member_keys)
+  end
+
+  def self.parse_member(member_key)
+    k, ts = member_key.split("#")
+
+    [k, Time.parse(ts)]
   end
 
   private
 
   def feed_key
     "feed:#{@user.id}"
+  end
+
+  def score(sleep_log)
+    sleep_log.sleep_duration
+  end
+
+  def member_key(sleep_log)
+    "#{sleep_log.id}##{sleep_log.created_at}"
   end
 end
--- app/services/timeline_service/precomputed_feed.rb
+++ app/services/timeline_service/precomputed_feed.rb
@@ -4,12 +4,37 @@ module TimelineService
       @user = user
     end
 
-    # TODO: filter out old logs
     def perform
       user_feed = UserFeed.new(@user)
-      logs = SleepLog.fetch_multi(user_feed.feed, includes: :user)
 
+      logs = user_feed.feed
+      deprecated_logs = remove_deprecated_logs(user_feed, logs)
+      filtered_logs = filter_logs(logs, deprecated_logs)
+
+      logs = SleepLog.fetch_multi(filtered_logs, includes: :user)
       logs.sort_by { |log| [log.sleep_duration, log.id] }.reverse
+    end
+
+    private
+
+    def remove_deprecated_logs(user_feed, logs)
+      deprecated_logs = logs.select do |log|
+        _, ts = UserFeed.parse_member(log)
+        ts < timeline_threshold
+      end
+
+      user_feed.remove_from_feed(deprecated_logs) if deprecated_logs.present?
+
+      deprecated_logs
+    end
+
+    def filter_logs(logs, deprecated_logs)
+      set_logs = deprecated_logs.to_set
+      logs.reject { |log| set_logs.include?(log) }
+    end
+
+    def timeline_threshold
+      7.days.ago.utc
     end
   end
 end
