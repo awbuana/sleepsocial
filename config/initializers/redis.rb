99d66ab3d67d96d618ae1bc9da0cb114b8241c49
Agni Wira Buana <agni.wira@bukalapak.com>
2025-06-27 17:52:10 +0700

    feat: add redis config to env.sample

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,2 @@
-REDIS_HOST = ENV.fetch("REDIS_HOST", "127.0.0.1")
-REDIS_DB = ENV.fetch("REDIS_DB", 0)
-
-redis_config = RedisClient.config(host: REDIS_HOST, port: 6379, db: REDIS_DB)
-REDIS = redis_config.new_pool(timeout: 0.5, size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5)))
+redis_config = RedisClient.config(host: ENV.fetch("REDIS_HOST", "127.0.0.1"), port: 6379, db: ENV.fetch("REDIS_DB", 0))
+REDIS = redis_config.new_pool(timeout: ENV.fetch("REDIS_TIMEOUT", 0.5).to_f, size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5)))
